92a1885f266f8fba9f9ea9638f30522c43c3bd0d
serenoulhes <bernardo.l.sigaia@gmail.com>
2023-11-16 17:49:44 -0300

    Versão 3 - Adicionei o print

--- exemplo2.php
+++ exemplo2.php
@@ -10,7 +10,7 @@
     <?php
         $x = 5;
         $y = 4;
-        echo "x=.$x;
+        echo "x=.$x";
         echo "<br> y=".$y;
         echo "<br> x+y= ".$x + $y;
         print("<br> teste");
